46c03b510a4210b5e9e32a073844c3a4e8f0c54a
Fabien MARTY <fabien.marty@gmail.com>
2019-08-19 13:31:04 +0200

    first try

---
+++ test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+IMAGE=$1
+BASE=$(echo "${IMAGE}" |awk -F ':' '{print $1;}')
+if test "${BASE}" = "debian"; then
+    BACKEND="apt-get"
+elif test "${BASE}" = "ubuntu"; then
+    BACKEND="apt-get"
+elif test "${BASE}" = "fedora"; then
+    BACKEND="yum"
+elif test "${BASE}" = "centos"; then
+    BACKEND="yum"
+elif test "${BASE}" = "opensuse/leap"; then
+    BACKEND="zypper"
+else
+    echo "ERROR: can't find a backend for ${BASE}"
+    exit 1
+fi
+
+if test "${BACKEND}" = "apt-get"; then
+    echo "***** PREPARING *****"
+    echo
+    if test "${BASE}" = "ubuntu"; then
+        cat /etc/apt/sources.list |grep -v '^#' |sed 's~http[^ ]* ~mirror://mirrors.ubuntu.com/mirrors.txt ~g' >/etc/apt/sources.list2
+        mv -f /etc/apt/sources.list2 /etc/apt/sources.list
+    fi
+    apt-get update
+    apt-get -y install apt-file
+    apt-file update
+    echo
+    echo
+fi
+
+echo "***** TESTING *****"
+echo
+
+RES=0
+for DEP in $(cat list.txt); do
+    if test "${BACKEND}" = "yum"; then
+        OUTPUT=$( ( yum provides --quiet --noplugins "${DEP}" 2>&1) |grep -v ^Error )
+        test "${OUTPUT}" != ""
+        NOT_FOUND=$?
+    elif test "${BACKEND}" = "apt-get"; then
+        apt-file search "${DEP}" >/dev/null 2>&1
+        NOT_FOUND=$?
+    elif test "${BACKEND}" = "zypper"; then
+        zypper se --provides  "${DEP}" >/dev/null 2>&1
+        NOT_FOUND=$?
+    fi
+    if test "${NOT_FOUND}" != "0"; then
+        echo "- Can't find: ${DEP} in ${IMAGE}"
+        RES=1
+    else
+        echo "- ok for ${DEP} in ${IMAGE}"
+    fi
+done
+
+exit ${RES}
